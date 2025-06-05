6f9b49b3f7a4f107b58cfdd7f6b77851423d5f7c
summit00 <finn_springer@aol.de>
2025-06-05 20:45:16 +0200

    add unit test for dc motor

--- tests/unitTests/all_tests.py
+++ tests/unitTests/all_tests.py
@@ -3,3 +3,4 @@ import pytest
 if __name__ == "__main__":
     # Explicitly specify the test files to run
     pytest.main(["tests/unitTests/pid_test.py"])
+    pytest.main(["tests/unitTests/dc_motor_test.py"])
